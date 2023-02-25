568c3efc3fbb221bb457d393b24bdc1d4f386d08
Tinymanx1 <msegreti02@gmail.com>
2023-02-25 17:11:48 -0500

    Reverted previous changes
    Changed away back to guest.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let homeScore = document.getElementById("home-score")
-let awayScore = document.getElementById("away-score")
-let awayCount = 0
+let guestScore = document.getElementById("guest-score")
+let guestCount = 0
 let homeCount = 0
 
 function add1PointsH() {
@@ -16,22 +16,22 @@ function add3PointsH() {
     homeScore.textContent = homeCount
 }
 
-function add1PointsA() {
-    awayCount += 1
-    awayScore.textContent = awayCount
+function add1PointsG() {
+    guestCount += 1
+    guestScore.textContent = guestCount
 }
-function add2PointsA() {
-    awayCount += 2
-    awayScore.textContent = awayCount
+function add2PointsG() {
+    guestCount += 2
+    guestScore.textContent = guestCount
 }
-function add3PointsA() {
-    awayCount += 3
-    awayScore.textContent = awayCount
+function add3PointsG() {
+    guestCount += 3
+    guestScore.textContent = guestCount
 }
 
 function reset() {
     homeScore.textContent = 0
-    awayScore.textContent = 0
+    guestScore.textContent = 0
     homeCount = 0
-    awayCount = 0
+    guestCount = 0
 }
